0c1faa0fdf93ca6c5b91f35e03db6b6b9049cee7
Texas-rangers <49830121+Texas-rangers@users.noreply.github.com>
2020-02-20 13:43:07 +0700

    Update sketch_feb17a.ino

--- sketch_feb17a.ino
+++ sketch_feb17a.ino
@@ -1,50 +1,52 @@
 #define Peizo D8
-//pass #define SoundSensor
+#define SoundSensor D10
 #define LED 13
-#define Button 8
 #define BLYNK_PRINT Serial
 
 #include <SPI.h>
 #include <ESP8266WiFi.h>
 #include <BlynkSimpleEsp8266.h>
-char auth[] = "7EmtwU9JoL84T-HHzFArr6Wh553rVUia";
-char ssid[] = "Heartk";
-char pass[] = "2345679pp";
+
+char auth[] = "";
+char ssid[] = "";
+char pass[] = "";
 
 unsigned long long FDetect=0,LDetect;
-int btn;
+int volume=0;
+const int TooLoud=80;
 BlynkTimer timer;
+WidgetLED led1(V1);
 
-void play(){
-  tone(Peizo,1000,500);
-}
-BLYNK_WRITE(V1){
-  btn = param.asInt();
-}
 void Main(){
-  //pass volume = digitalRead(SoundSensor);
-  if(/*volume > 100*/btn == HIGH){
+  volume = analogRead(SoundSensor);
+  if(volume > TooLoud){
       digitalWrite(LED,HIGH);
       LDetect = millis();
+      led1.on();
       if(FDetect == 0){
         FDetect = millis();
       }
       if(LDetect - FDetect > 3000){
-        play();
+        tone(Peizo,1000,500);
       }
   }else{
       if(millis() - LDetect < 1500){
           digitalWrite(LED,LOW);
+          led1.off();
           FDetect = 0;
       } 
+  }
+  if(millis() - LDetect < 20000){
+    Blynk.virtualWrite(V2, (millis() - LDetect)/1000);
+  }else{
+    Blynk.virtualWrite(V2, "Room is quite");
   }
 }
 
 void setup(){
   Serial.begin(9600);
-  //pass pinMode(SoundSensor, INPUT);
+  pinMode(SoundSensor, INPUT);
   pinMode(LED, OUTPUT);
-  pinMode(Button, INPUT);
   Blynk.begin(auth, ssid ,pass);
   timer.setInterval(500L, Main);
 }
